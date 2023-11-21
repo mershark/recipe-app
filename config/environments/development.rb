c62fee42ae576e29b7861e3548f7989f7e0e8c76
mershark <mersharktakyi@gmail.com>
2023-11-21 01:32:15 +0000

    Setup login and signup with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -16,6 +19,8 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
