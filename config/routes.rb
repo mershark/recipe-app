6790c4c356ca6b2b1f6161ca84f455a12ab7f267
aqurds <omarf1320@gmail.com>
2023-11-22 16:50:36 +0600

    Add recipe list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
   devise_for :users
   root to: "users#index"
 
   resources :foods, only: [:index, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy]
 end
